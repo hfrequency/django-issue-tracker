8cae4bf31880b3a6b786317f66e914fbc710838b
Cesar Otero <hfrequency@gmail.com>
2012-06-08 16:26:45 -0700

    Added a direct to template render for the index.

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('django.views.generic.simple',
     # Examples:
+    (r'^$', 'direct_to_template', {
+        'template': 'index.html'
+    })
     # url(r'^$', 'issue_tracker.views.home', name='home'),
     # url(r'^issue_tracker/', include('issue_tracker.foo.urls')),
 

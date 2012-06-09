66f06164a5654f2925fb16a1ce28638fd57e3a9e
Cesar Otero <hfrequency@gmail.com>
2012-06-08 19:50:46 -0700

    Add url mapping to register.

--- issue_tracker/accounts/urls.py
+++ issue_tracker/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.views import logout_then_login, login
+from accounts.views import register
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 
 urlpatterns = patterns('',
+    (r'^register/$', register, {}, 'register' ),
     (r'^login/$', login, {}, 'login' ),
         (r'^logout/$', logout_then_login, {}, 'logout'),
 )

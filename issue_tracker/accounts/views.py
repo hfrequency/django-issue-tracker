5570617cdbbde632949dbbbf4562031231219875
Cesar Otero <hfrequency@gmail.com>
2012-06-08 19:49:54 -0700

    Added register callback method in accounts/views.py for handling registration of new users.

--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/account/")
+    else:
+        form = UserCreationForm()
+    return render_to_response("registration/register.html", {
+        'form': form,
+    })

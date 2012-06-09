26e4566fb1036ad5b348f66f493894360acb74ff
Cesar Otero <hfrequency@gmail.com>
2012-06-08 19:01:58 -0700

    Changed login url to use default url mapping. Reminder: Django looks in registration/login.html by default.

--- issue_tracker/accounts/urls.py
+++ issue_tracker/accounts/urls.py
@@ -3,6 +3,6 @@ from django.contrib.auth.views import logout_then_login, login
 from django.contrib.auth.forms import AuthenticationForm
 
 urlpatterns = patterns('',
-    (r'^login/$', login, {'authentication_form': AuthenticationForm}, 'login'),
+    (r'^login/$', login, {}, 'login' ),
         (r'^logout/$', logout_then_login, {}, 'logout'),
 )

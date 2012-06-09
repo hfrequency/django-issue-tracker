7ce9aa6a9580933978deeddca0d40858dd552650
Cesar Otero <hfrequency@gmail.com>
2012-06-09 00:32:15 -0700

    Changed redirect value.

--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -12,7 +12,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            return HttpResponseRedirect("/account/")
+            return HttpResponseRedirect("/issues/")
     else:
         form = UserCreationForm()
     return render_to_response("registration/register.html", {

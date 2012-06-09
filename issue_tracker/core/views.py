37d73bac2c22059e16b2e056d25962507fa0549c
Cesar Otero <hfrequency@gmail.com>
2012-06-09 00:53:30 -0700

    Added login required decorator

--- issue_tracker/core/views.py
+++ issue_tracker/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login/')
 def issues(request):
     return render_to_response("issues.html", {})

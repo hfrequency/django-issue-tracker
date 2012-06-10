d847c22c5a04b8b6665ce5af3929776796d886a1
Cesar Otero <hfrequency@gmail.com>
2012-06-09 19:16:25 -0700

    Enabled the admin panel.

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('django.views.generic.simple',
     # Examples:
@@ -19,6 +17,5 @@ urlpatterns = patterns('django.views.generic.simple',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )

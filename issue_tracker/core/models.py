7c5abb3349b96baddfb580434e2a8febf41a1c66
Cesar Otero <hfrequency@gmail.com>
2012-06-09 19:12:56 -0700

    Added Comments, Issues, Status, Level, and Project models. This is the foundation for the project.

--- issue_tracker/core/models.py
+++ issue_tracker/core/models.py
@@ -1,2 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Comments(models.Model):
+    comment = model.CharField(max_length=500)
+    user = model.ForeignKey(User)
+
+class Level(models.Model):
+    level_choices = (
+        (0, "LOW"),
+        (1, "MEDIUM"),
+        (2, "HIGH"),
+        (3, "CRITICAL"),
+        (4, "BLOCKER"),
+    )
+    level = models.CharField(max_length=10, choices=level_choices)
+
+class Status(models.Model):
+    status_choices = (
+        (0, "OPEN"),
+        (1, "IN PROGRESS"),
+        (2, "FINISHED"),
+        (3, "CLOSED"),
+        (4, "CANCELED"),
+    )
+    status = models.CharField(max_length=10, choices=status_choices)
+
+class Issue(models.Model):
+    project = models.ForeignKey(Project)
+    status = models.ForeignKey(Status)
+    level = models.ForeignKey(Level)
+    comments = models.ForeignKey(Comments)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    priority = models.CharField(max_length=10)  
+    date_created = models.DateField() 
+    date_completed = models.DateField() 
+    # TODO implement these
+    # time_estimate 
+    # percentage_completed  
+
+class Project(models.Model):
+    user = model.ForeignKey(User)
+    issue = models.ForeignKey(Issue)
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=15)
+    release_date = models.DateField()
 

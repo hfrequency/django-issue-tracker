6ad82ebc4d526b77fec36412c98b19547f3242bc
Cesar Otero <hfrequency@gmail.com>
2012-06-10 22:07:04 -0700

    Changed issues to projects.html which is where a person should land after logging in.

--- issue_tracker/core/views.py
+++ issue_tracker/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from core.models import *
 
 @login_required(login_url='/login/')
 def issues(request):
-    return render_to_response("issues.html", {})
+    projects = Project.objects.all() 
+    return render_to_response("projects.html", {
+        'projects' : projects,
+    })

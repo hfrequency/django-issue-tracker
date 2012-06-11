2f6ff3348e73ea197b91808363b8ec8a020fb432
Cesar Otero <hfrequency@gmail.com>
2012-06-10 22:15:19 -0700

    Changed mapping from issues to project; and the mapping now accepts a project_id parameter.

--- issue_tracker/core/urls.py
+++ issue_tracker/core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from core.views import issues
 
 urlpatterns = patterns('',
-    (r'^issues/$', issues, {}, 'issues' ),
+    (r'^projects/(?P<project_id>\d+)/$', projects, {}, 'projects' ),
 )
 

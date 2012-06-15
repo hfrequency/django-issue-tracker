32e29e0295b27278697966d512f3e63fcbad6b2b
Cesar Otero <hfrequency@gmail.com>
2012-06-14 20:12:26 -0700

    Removed project_details, and added code for querying the current user in the session, creating an inlineformset of Projects, and either saving or displaying the current formset depending on whether the request type is a post or a get. In summary, the callback function now handles saving project forms.

--- issue_tracker/core/views.py
+++ issue_tracker/core/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from core.models import *
+from django.forms.models import modelformset_factory, inlineformset_factory
+from django.views.decorators.csrf import csrf_protect
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from core.forms import *
 
 @login_required(login_url='/login/')
+@csrf_protect
 def projects(request):
-    projects = Project.objects.all() 
-    return render_to_response("projects.html", {
-        'projects' : projects,
-    })
 
-@login_required(login_url='/login/')
-def project_detail(request, project_id):
-    project = Project.objects.get(id=project_id) 
+    project = Project.objects.all()
+
+    # TODO check if this is the best way to get the current user in session.
+    user = User.objects.get(username=request.META["USER"])
+
+    ProjectFormSet = inlineformset_factory(User, Project, max_num=10, extra=1, \
+        fields = ('name', 'version', 'release_date'))
+        
+
+    if request.method == 'POST':
+        formset = ProjectFormSet(request.POST, instance=user)
+        if formset.is_valid():
+            formset.save()
+            return HttpResponseRedirect('/projects/')
+    else:
+        formset = ProjectFormSet(instance=user) 
+
     return render_to_response("projects.html", {
-        'project' : project,
-    })
+        'formset' : formset
+    }, context_instance=RequestContext(request))
+

9c25035e7b08581103110a3fad52b39970e568e9
Cesar Otero <hfrequency@gmail.com>
2012-06-16 23:50:52 -0700

    Added url mapping to comments.

--- issue_tracker/core/urls.py
+++ issue_tracker/core/urls.py
@@ -4,6 +4,5 @@ from core.views import projects, issues
 urlpatterns = patterns('',
     (r'^projects/$', projects, {}, 'projects' ),
     (r'^issues/(?P<project_id>\d+)/$', issues, {}, 'issues' ),
-    (r'^comments/(?P<issue_id>\d+)/$', comments, {}, 'comments' ),
 )
 

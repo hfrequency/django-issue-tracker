e73edb608858c20726e48e12ec3f33e302af36e7
Cesar Otero <hfrequency@gmail.com>
2012-06-16 23:04:42 -0700

    Removed the Status model, and updated model definitions for Project and Issue models.

--- issue_tracker/core/models.py
+++ issue_tracker/core/models.py
@@ -15,33 +15,37 @@ class Level(models.Model):
     )
     level = models.CharField(max_length=10, choices=level_choices)
 
-class Status(models.Model):
-    status_choices = (
-        (0, "OPEN"),
-        (1, "IN PROGRESS"),
-        (2, "FINISHED"),
-        (3, "CLOSED"),
-        (4, "CANCELED"),
-    )
-    status = models.CharField(max_length=10, choices=status_choices)
-
-class Issue(models.Model):
-    status = models.ForeignKey(Status)
-    level = models.ForeignKey(Level)
-    comments = models.ForeignKey(Comments, null=True)
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
-    priority = models.CharField(max_length=10)  
-    date_created = models.DateField() 
-    date_completed = models.DateField() 
-    # TODO implement these
-    # time_estimate 
-    # percentage_completed  
-
 class Project(models.Model):
     user = models.ForeignKey(User)
-    issue = models.ForeignKey(Issue, null=True)
+    # issue = models.ForeignKey(Issue, null=True)
     name = models.CharField(max_length=100)
     version = models.CharField(max_length=15, null=True)
     release_date = models.DateField(null=True)
+
+class Issue(models.Model):
+    project = models.ForeignKey(Project)
+    status_choices = (
+        ("0", "OPEN"),
+        ("1", "IN PROGRESS"),
+        ("2", "FINISHED"),
+        ("3", "CLOSED"),
+        ("4", "CANCELED"),
+    )
+    status = models.CharField(max_length=10, choices=status_choices)
+    level_choices = (
+        ("0", "LOW"),
+        ("1", "MEDIUM"),
+        ("2", "HIGH"),
+        ("3", "CRITICAL"),
+        ("4", "BLOCKER"),
+    )
+    level = models.CharField(max_length=10, choices=level_choices)
+    comments = models.ForeignKey(Comments, null=True)
+    title = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=50, null=True)
+    date_created = models.DateField(auto_now_add=True) 
+    date_completed = models.DateField(null=True) 
+    # TODO implement these
+    # time_estimate 
+    # percentage_completed  
 

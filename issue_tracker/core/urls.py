8db75b1b9a996796cb80e159ba4a102386616d71
Cesar Otero <hfrequency@gmail.com>
2012-06-16 23:00:46 -0700

    Added a url mapping to issues, where the issue list is resolved using the project id.

--- issue_tracker/core/urls.py
+++ issue_tracker/core/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
-from core.views import projects 
+from core.views import projects, issues 
 
 urlpatterns = patterns('',
     (r'^projects/$', projects, {}, 'projects' ),
+    (r'^issues/(?P<project_id>\d+)/$', issues, {}, 'issues' ),
 )
 

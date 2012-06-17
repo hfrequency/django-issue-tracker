02b7b5b925e43ce61fa6126703be9d1ad91d8c82
Cesar Otero <hfrequency@gmail.com>
2012-06-17 15:09:43 -0700

    The project_id is now passed as a parameter to comments. This parameter is needed for linking back to the issues page.

--- issue_tracker/core/urls.py
+++ issue_tracker/core/urls.py
@@ -4,6 +4,6 @@ from core.views import projects, issues, comments
 urlpatterns = patterns('',
     (r'^projects/$', projects, {}, 'projects' ),
     (r'^issues/(?P<project_id>\d+)/$', issues, {}, 'issues' ),
-    (r'^comments/(?P<issue_id>\d+)/$', comments, {}, 'comments' ),
+    (r'^comments/(?P<project_id>\d+)/(?P<issue_id>\d+)/$', comments, {}, 'comments' ),
 )
 

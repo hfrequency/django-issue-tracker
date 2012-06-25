210c5cb91372adf6f4b52bd651dfebe78fc0d69d
Cesar Otero <hfrequency@gmail.com>
2012-06-24 18:09:10 -0700

    Added accounts as an installed app.

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'core',
+    'accounts',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
